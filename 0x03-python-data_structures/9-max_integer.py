50f3787b9b13ce7de3829c82760832f3076e6859
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-03-14 17:22:32 -0700

    python data structures

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    largest_num = my_list[0]
+    for num in my_list:
+        if num > largest_num:
+            num = largest_num
+            return (largest_num)
