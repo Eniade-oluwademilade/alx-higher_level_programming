9c0916f1b0d7bfc2822f1103d361f03dfe13d50b
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-03-18 13:58:00 -0700

    dictionaries in python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multipy_by_2(a_dictionary):
-    new_dict = {}
-    for keys in a_dictionary:
-        new_dict += keys*2
+    new_dict = a_dictionary.copy()
+    for i in a_dictionary.keys():
+        new_dict[i] *= 2
         return (new_dict)
