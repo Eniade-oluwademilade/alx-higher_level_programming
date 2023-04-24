ade41c63a8f8804c7750b2b38d7965dc7731e30e
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-04-24 14:44:29 -0700

    json representation of object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns json representation of string"""
+
+
+def to_json_string(my_obj):
+    """Function returns json representation of my_obj"""
+    return json.dumps(my_obj)
