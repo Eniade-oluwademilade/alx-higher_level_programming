c0877a992cf3b7ce9a3e31fa705ce15d1fc3de96
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-04-24 14:15:48 -0700

    input output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function writes string to text file"""
+
+
+def write_file(filename="", text=""):
+    """Write string to file and return number of characters
+        filename: name of file to write to
+        text: string to write in file
+    """
+    with open(filename, encoding="utf-8") as f:
+        return (f.write(text))
